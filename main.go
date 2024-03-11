990ecea39e84e7b5b9f9bf4a0f0f388c0c5a5524
DerMaddis <eulermat16@gmail.com>
2024-03-11 20:02:33 +0100

    Added basic parser for rooms and hours

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/dermaddis/sub_plan/util"
+	_ "github.com/dermaddis/sub_plan/util"
 	"github.com/joho/godotenv"
 )
 
@@ -20,6 +22,7 @@ var myClasses = []string{
 }
 
 var notFoundError = errors.New("page not found")
+var parseError = errors.New("Failed parsing")
 
 func main() {
 	err := godotenv.Load()
@@ -87,6 +90,24 @@ func requestSubst(baseUrl, sessionId string, n int) (string, error) {
 	return htmlString, nil
 }
 
+type EntryType int
+
+const (
+	Cancelled = 0
+	Switch    = 1
+	Other     = 2
+)
+
+type Entry struct {
+	Texts [7]string
+	Type  EntryType
+	Room  string
+	Class string
+	Date  time.Time
+	Start int
+	End   int
+}
+
 func parseHtml(doc *goquery.Document) {
 	titleSelection := doc.Find(".mon_title")
 	if titleSelection.Length() == 0 {
@@ -104,18 +125,72 @@ func parseHtml(doc *goquery.Document) {
 			texts = append(texts, s.Text())
 		})
 
-		hours := texts[2]
-		room := texts[3]
-		className := texts[4]
+		log.Println("---", texts, "---")
 
-		compare := func(first string) func(other string) bool {
-			return func(other string) bool {
-				return first == other
+		hoursStr := texts[2]
+		roomStr := texts[3]
+		// className := texts[4]
+
+		start, end, err := parseHours(hoursStr)
+		if err != nil {
+			if errors.Is(err, parseError) {
+				start, end = 0, 0
+			} else {
+				log.Fatalln(err)
 			}
 		}
+		log.Println("start:", start, "end:", end)
 
-		if util.Some(myClasses, compare(className)) {
-			log.Println(className, "in", room, "at", hours)
-		}
+		entryType, roomData1, roomData2 := parseRoom(roomStr)
+		log.Println("entryType:", entryType, "roomData:", roomData1, roomData2)
 	})
+}
+
+func parseHours(s string) (int, int, error) {
+	switch len(s) {
+	case 1:
+		res, err := strconv.Atoi(s)
+		if err != nil {
+			return 0, 0, parseError
+		}
+		return res, 0, nil
+	default:
+		split := strings.Split(s, " - ")
+		if len(split) != 2 {
+			return 0, 0, parseError
+		}
+
+		first, err := strconv.Atoi(split[0])
+		if err != nil {
+			return 0, 0, parseError
+		}
+
+		second, err := strconv.Atoi(split[1])
+		if err != nil {
+			return 0, 0, parseError
+		}
+
+		return first, second, nil
+	}
+}
+
+func parseRoom(s string) (EntryType, string, string) {
+	switch {
+	case strings.HasSuffix(s, "---"):
+		return Cancelled, "", ""
+	case strings.Contains(s, "→"):
+		rooms := strings.Split(s, "→")
+		if len(rooms) != 2 {
+			return Other, s, ""
+		}
+		return Switch, rooms[0], rooms[1]
+	default:
+		return Other, s, ""
+	}
+}
+
+func compare(first string) func(other string) bool {
+	return func(other string) bool {
+		return first == other
+	}
 }
